6b5371357fbd9706d1b331629cd09798586e5b17
TyTPa <papushina.tatyana@gmail.com>
2025-05-04 20:05:17 +0300

    третье задание

--- keyboards.py
+++ keyboards.py
@@ -2,33 +2,29 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMar
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 # Клавиатура внизу чата
-
 main = ReplyKeyboardMarkup(keyboard=[
-   [KeyboardButton(text="Привет")],
-   [KeyboardButton(text="Пока")]
+    [KeyboardButton(text="Привет")],
+    [KeyboardButton(text="Пока")]
 ], resize_keyboard=True)
 
-#Клавиатура под сообщением
-
-#для Задания 2
+# Клавиатура под сообщением для задания 2
 inline_keyboard_test = InlineKeyboardMarkup(inline_keyboard=[
-   [InlineKeyboardButton(text="Новости", url='https://www.rbc.ru/')],
-   [InlineKeyboardButton(text="Музыка", url='https://music.yandex.ru/')],
-   [InlineKeyboardButton(text="Видео", url='https://rutube.ru/')]
+    [InlineKeyboardButton(text="Новости", url='https://www.rbc.ru/')],
+    [InlineKeyboardButton(text="Музыка", url='https://music.yandex.ru/')],
+    [InlineKeyboardButton(text="Видео", url='https://rutube.ru/')]
 ])
 
-#для задания 3
+# Клавиатура под сообщением для задания 3
 inline_keyboard_more = InlineKeyboardMarkup(inline_keyboard=[
-   [InlineKeyboardButton(text="Показать больше", callback_data='more')],
-
+    [InlineKeyboardButton(text="Показать больше", callback_data='more')]
 ])
 
-#Третий способ создания клавиатуры — билдер
+# Третий способ создания клавиатуры — билдер
 test = ["Опция 1", "Опция 2"]
 
-# inline клавиатура
+# Inline клавиатура
 async def dyn_keyboard():
     keyboard = InlineKeyboardBuilder()
     for key in test:
-        keyboard.add(InlineKeyboardButton(text=key))
-    return keyboard.adjust(2).as_markup()
+        keyboard.add(InlineKeyboardButton(text=key, callback_data=key.lower()))  # Добавляем callback_data
+    return keyboard.adjust(1).as_markup()
--- main.py
+++ main.py
@@ -4,55 +4,57 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, FSInputFile, CallbackQuery
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 import random
-
 from gtts import gTTS
 import os
-
 from config import TOKEN
-
+import keyboards as kb
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-import keyboards as kb
-
 @dp.message(CommandStart())
 async def start(message: Message):
-# Вызов клавиатуры внизу
-   await message.answer(f'Приветики, {message.from_user.first_name}', reply_markup=kb.main)
-# Вызов inline клавиатуры
-#   await message.answer(f'Приветики, {message.from_user.first_name}', reply_markup=kb.inline_keyboard_test)
-# Вызов билдера
-#   await message.answer(f'Приветики, {message.from_user.first_name}', reply_markup=await kb.test_keyboard())
+    # Вызов клавиатуры внизу
+    await message.answer(f'Приветики, {message.from_user.first_name}', reply_markup=kb.main)
+    # Вызов inline клавиатуры
+    # await message.answer(f'Приветики, {message.from_user.first_name}', reply_markup=kb.inline_keyboard_test)
+    # Вызов билдера
+    # await message.answer(f'Приветики, {message.from_user.first_name}', reply_markup=await kb.test_keyboard())
 
-# Обработка клавиатуры с replay кнопками
-dp.message(F.text == "Привет")
+# Обработка клавиатуры с reply кнопками
+@dp.message(F.text == "Привет")
 async def test_button(message: Message):
-   await message.answer(f' Привет, {message.from_user.first_name}',reply_markup=kb.main)
+    await message.answer(f'Привет, {message.from_user.first_name}')
 
-@ dp.message(F.text == "Пока")
+@dp.message(F.text == "Пока")
 async def test_button(message: Message):
-   await message.answer(f' Пока, {message.from_user.first_name}')
+    await message.answer(f'Пока, {message.from_user.first_name}')
 
 # Вызов inline клавиатуры
-
 @dp.message(Command(commands=['link']))
 async def link_command(message: Message):
-   await message.answer(f'Привет, {message.from_user.first_name}', reply_markup=kb.inline_keyboard_test)
+    await message.answer(f'Привет, {message.from_user.first_name}', reply_markup=kb.inline_keyboard_test)
 
-# Обработка клавиатуры с inline кнопками
+# Обработка клавиатуры с inline кнопками и билдером
 @dp.message(Command(commands=['dynamic']))
 async def dynamic_command(message: Message):
-   await message.answer(f'Привет, {message.from_user.first_name}', reply_markup= kb.inline_keyboard_more)
+    await message.answer(f'Привет, {message.from_user.first_name}', reply_markup=kb.inline_keyboard_more)
 
 @dp.callback_query(F.data == 'more')
-async def news(callback: CallbackQuery):
-   await callback.answer("Подгружаем", show_alert=True)
-   await callback.message.edit_text('Вот свежие новости!', reply_markup=await kb.dyn_keyboard())
+async def more(callback: CallbackQuery):
+    await callback.answer("Подгружаем", show_alert=True)
+    await callback.message.edit_text('Вот свежие новости!', reply_markup=await kb.dyn_keyboard())
 
+@dp.callback_query(F.data == 'опция 1')
+async def option_1(callback: CallbackQuery):
+    await callback.message.answer(f'Вы выбрали Опцию 1, {callback.from_user.first_name}')
+
+@dp.callback_query(F.data == 'опция 2')
+async def option_2(callback: CallbackQuery):
+    await callback.message.answer(f'Вы выбрали Опцию 2, {callback.from_user.first_name}')
 
 async def main():
-   await dp.start_polling(bot)
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
-   asyncio.run(main())
+    asyncio.run(main())
